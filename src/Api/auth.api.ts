68debec881342078ba8ce87fe092cd9ab0a248ed
Illia Sheverdinkin <illia.sheverdinkin@chisw.com>
2023-09-29 21:32:08 +0300

    add logs

--- src/Api/auth.api.ts
+++ src/Api/auth.api.ts
@@ -11,16 +11,15 @@ import Cookies from 'js-cookie';
 export const AuthApi = {
   authorize: async (authData: IAuthorize): Promise<IAuthState> => {
     try {
-      const auth = (await instance.post<IAuth>('auth/login', authData));
-      console.log(Cookies.get('accessToken'), auth)
-      // if (auth.isAuthorized && auth.name && auth.accessToken) {
-      //   Cookies.set('isAuthorized', auth.isAuthorized.toString());
-      //   Cookies.set('userName', auth.name);
-      //   Cookies.set('accessToken', auth.accessToken);
-      // }
+      const auth = (await instance.post<IAuth>('auth/login', authData)).data;
+      if (auth.isAuthorized && auth.name && auth.accessToken) {
+        Cookies.set('isAuthorized', auth.isAuthorized.toString());
+        Cookies.set('userName', auth.name);
+        Cookies.set('accessToken', auth.accessToken);
+      }
 
       return {
-        auth: auth.data,
+        auth,
       };
     } catch (e) {
       const error = e as AxiosError<any>;
